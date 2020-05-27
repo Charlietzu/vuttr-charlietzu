5c83329a221a2b2a2d8addfd292ff435c39e3bc6
Caio César Silva <caiocesarsilva08@gmail.com>
2020-05-26 22:46:01 -0300

    Add and Remove Tool and ReactStrap
    I implemented the reactstrap lib in the application, so it reduces the verbosity of the layout code.
    
    I also implemented the addTool and removeTool functions. I still need to treat some fields, like the link input (so it record the "http://" before the string, if it already doesn't have it) , and the tags,(so it shows the tags being added to the tags array).

--- src/App.js
+++ src/App.js
@@ -1,6 +1,17 @@
 import React, { Component } from "react";
 import "./App.css";
-import { render } from "@testing-library/react";
+import {
+  Form,
+  Button,
+  Modal,
+  ModalBody,
+  ModalHeader,
+  ModalFooter,
+  FormGroup,
+  Input,
+  Label,
+} from "reactstrap";
+
 import axios from "axios";
 const API_URL = "http://localhost:3000/tools";
 
@@ -9,6 +20,15 @@ class App extends Component {
     super(props);
     this.state = {
       tools: [],
+      newToolModal: false,
+      newToolData: {
+        title: "",
+        description: "",
+        link: "",
+        tagsText: "",
+        tagspreparation: [],
+        tags: [],
+      },
     };
   }
 
@@ -16,13 +36,48 @@ class App extends Component {
     axios
       .get(API_URL)
       .then((response) => {
-        console.log(response.data);
         this.setState({ tools: response.data });
-        console.log(this.state.tools);
       })
       .catch(function (error) {
         console.log(error);
       });
+  }
+
+  addTool() {
+    axios.post(API_URL, this.state.newToolData).then((response) => {
+      let { tools } = this.state;
+      tools.push(response.data);
+      this.setState({
+        tools,
+        newToolData: {
+          title: "",
+          description: "",
+          link: "",
+          tagspreparation: [],
+          tags: [],
+        },
+      });
+      this.toggleNewToolModal();
+    });
+  }
+
+  deleteTool(id) {
+    axios.delete(API_URL + "/" + id).then((response) => {
+      this.getTools();
+    });
+  }
+
+  toggleNewToolModal() {
+    this.setState({
+      newToolModal: !this.state.newToolModal,
+      newToolData: {
+        title: "",
+        description: "",
+        link: "",
+        tagspreparation: [],
+        tags: [],
+      },
+    });
   }
 
   componentDidMount() {
@@ -30,7 +85,30 @@ class App extends Component {
   }
 
   render() {
-    //let tools = this.state.tools.map((tool) => {})
+    let tools = this.state.tools.map((tool) => {
+      return (
+        <div key={tool.id} className="card mb-2">
+          <a href={tool.link}>
+            <h5 className="card-header card-title">{tool.title}</h5>
+          </a>
+          <div className="card-body">
+            <p className="card-text">{tool.description}</p>
+            <p className="card-text">
+              {tool.tags.map((tag) => {
+                return "#" + tag + " ";
+              })}
+            </p>
+            <button
+              onClick={this.deleteTool.bind(this, tool.id)}
+              className="btn btn-danger"
+            >
+              Remove
+            </button>
+          </div>
+        </div>
+      );
+    });
+
     return (
       <>
         <div className="container-fluid">
@@ -39,16 +117,113 @@ class App extends Component {
             <h3>Very Useful Tools to Remember</h3>
           </div>
         </div>
-        <div className="card">
-          <a href="#">
-            <h5 className="card-header card-title">Title</h5>
-          </a>
-          <div className="card-body">
-            <p className="card-text">Description</p>
-            <p className="card-text">Tags</p>
-            <button className="btn btn-danger">Remove</button>
-          </div>
+        <div className="container mb-4">
+          <Button onClick={this.toggleNewToolModal.bind(this)} color="primary">
+            Add Tool
+          </Button>
         </div>
+        <div className="container">{tools}</div>
+
+        <Modal
+          isOpen={this.state.newToolModal}
+          toggle={this.toggleNewToolModal.bind(this)}
+        >
+          <ModalHeader>Add a new Tool</ModalHeader>
+          <ModalBody>
+            <Form>
+              <FormGroup>
+                <Label for="title">Title</Label>
+                <Input
+                  type="text"
+                  placeholder="Tool Title"
+                  className="form-control"
+                  id="title"
+                  value={this.state.newToolData.title}
+                  onChange={(e) => {
+                    let { newToolData } = this.state;
+                    newToolData.title = e.target.value;
+                    this.setState({ newToolData });
+                  }}
+                />
+              </FormGroup>
+              <FormGroup>
+                <Label for="link">Link</Label>
+                <Input
+                  type="text"
+                  className="form-control"
+                  id="link"
+                  aria-describedby="linkHelp"
+                  value={this.state.newToolData.link}
+                  onChange={(e) => {
+                    let { newToolData } = this.state;
+                    newToolData.link = e.target.value;
+                    this.setState({ newToolData });
+                  }}
+                />
+                <small id="linkHelp" className="form-text text-muted">
+                  Format: http://link.com
+                </small>
+              </FormGroup>
+              <FormGroup>
+                <Label for="description">Description</Label>
+                <Input
+                  type="text"
+                  className="form-control"
+                  id="description"
+                  value={this.state.newToolData.description}
+                  onChange={(e) => {
+                    let { newToolData } = this.state;
+                    newToolData.description = e.target.value;
+                    this.setState({ newToolData });
+                  }}
+                />
+              </FormGroup>
+              <FormGroup>
+                <Label for="tags">Tags</Label>
+                <Input
+                  type="text"
+                  className="form-control"
+                  id="tags"
+                  aria-describedby="tagsHelp"
+                  value={this.state.newToolData.tagsText}
+                  onKeyDown={(e) => {
+                    let { newToolData } = this.state;
+                    if (e.keyCode === 32) {
+                      newToolData.tags.push(e.target.value.trim());
+                      //newToolData.tagspreparation.push(e.target.value);
+                      /*newToolData.tags = newToolData.tags.concat(
+                        newToolData.tagspreparation
+                      );*/
+                      console.log("tags: ", newToolData.tags);
+                      e.target.value = "";
+                      e.target.focus();
+                    }
+                  }}
+                  onChange={(e) => {
+                    let { newToolData } = this.state;
+                    newToolData.tagsText = e.target.value;
+                    this.setState({ newToolData });
+                  }}
+                />
+                <small id="tagsHelp" className="form-text text-muted">
+                  Split the tags using the space bar.
+                </small>
+              </FormGroup>
+            </Form>
+          </ModalBody>
+          <ModalFooter>
+            {" "}
+            <Button
+              color="secondary"
+              onClick={this.toggleNewToolModal.bind(this)}
+            >
+              Cancel
+            </Button>
+            <Button color="primary" onClick={this.addTool.bind(this)}>
+              Add Tool
+            </Button>
+          </ModalFooter>
+        </Modal>
       </>
     );
   }
