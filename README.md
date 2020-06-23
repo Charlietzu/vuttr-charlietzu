da9cbc162cb4aa3e92ddfaec990a45ecb3216d91
Caio César Silva <caiocesarsilva08@gmail.com>
2020-06-22 21:36:17 -0300

    Updated README and fixed a toolcard key warning bug.

--- README.md
+++ README.md
@@ -14,10 +14,20 @@ And:
 
 ### `npm run dev`
 
-This will open two terminals, on of them will start the application at:
+This will open two terminals, one of them will start the application at:
 
 ### `http://localhost:8000`
 
 And the other will also start the API at:
 
 ### `http://localhost:3000`
+
+### DEPENDENCIES USED IN THIS APPLICATION
+
+- bootstrap/react-bootstrap/reactstrap: libraries used to create the responsive interface.
+- chai: library used to create assertions in the application tests, i think it is better than the native assertions.
+- deep-freeze: library used for recursive call of the expression Object.freeze, i used it to maintain the principle of immutability.
+- react-redux/redux/redux-immutable-state-invariant/redux-thunk: libraries used to control and manipulate the state of the application, it makes the implementation of new actions in the project more easier.
+- react-router-dom: library used for the application routing.
+- json-server: library used for creating a local server for the JSON data file.
+- cross-env: library used for running scripts that set and use environment variables across platforms.
--- src/components/tools/ToolCard.js
+++ src/components/tools/ToolCard.js
@@ -10,32 +10,30 @@ import "./tools.css";
 const ToolCard = ({ tools, onDeleteClick }) =>
   tools.map((tool) => {
     return (
-      <>
-        <Card key={tool.id} className="m-3 cardBox">
-          <CardTitle className="mt-3 ml-3">
-            <h5 style={{ fontWeight: "bold" }}>
-              <a href={tool.link} target="_blank">
-                {tool.title}
-              </a>
-            </h5>
-          </CardTitle>
+      <Card key={tool.id} className="m-3 cardBox">
+        <CardTitle className="mt-3 ml-3">
+          <h5 style={{ fontWeight: "bold" }}>
+            <a href={tool.link} target="_blank">
+              {tool.title}
+            </a>
+          </h5>
+        </CardTitle>
 
-          <CardBody>
-            <CardText>{tool.description}</CardText>
-            <CardText style={{ fontWeight: "bold" }}>
-              {tool.tags.map((tag) => {
-                return "#" + tag + " ";
-              })}
-            </CardText>
-            <Button
-              className="m-1 buttonRemove"
-              onClick={() => onDeleteClick(tool)}
-            >
-              Remove
-            </Button>
-          </CardBody>
-        </Card>
-      </>
+        <CardBody>
+          <CardText>{tool.description}</CardText>
+          <CardText style={{ fontWeight: "bold" }}>
+            {tool.tags.map((tag) => {
+              return "#" + tag + " ";
+            })}
+          </CardText>
+          <Button
+            className="m-1 buttonRemove"
+            onClick={() => onDeleteClick(tool)}
+          >
+            Remove
+          </Button>
+        </CardBody>
+      </Card>
     );
   });
 
